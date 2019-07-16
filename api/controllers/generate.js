89c2406fbcb62c453f6253ce80096c9cae48916d
JABvzla <joseantoniobonito@hotmail.com>
2019-07-15 22:39:36 -0300

    feat: clean old models

--- api/controllers/generate.js
+++ api/controllers/generate.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
-module.exports = {
+const MODELS_PATH = 'api/models/';
+const excludeFiles = ['.gitkeep', 'Models.js'];
 
+module.exports = {
   friendlyName: 'Generate',
 
   description: 'Generate models with data from database.',
@@ -10,14 +12,23 @@ module.exports = {
 
   exits: {},
 
-  fn: async function (inputs) {
+  fn: async function() {
     const models = await Models.find();
-    models.forEach( model => {
-      const content = `module.exports = ${JSON.stringify(model.attributes)}`;
 
-      fs.writeFile(`api/models/${model.name}.js`, content, () => {});
+    fs.readdir(MODELS_PATH, (err, files) => {
+      if (err) return;
+
+      files
+        .filter(e => !e.indexOf(excludeFiles))
+        .forEach(e => fs.unlink(`${MODELS_PATH}${e}`, () => {}));
     });
 
-    return;
+    models.forEach(model => {
+      const content = `module.exports = ${JSON.stringify(model.attributes)}`;
+
+      fs.writeFile(`${MODELS_PATH}${model.name}.js`, content, () => {});
+    });
+
+    return models;
   }
 };
